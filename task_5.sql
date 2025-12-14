5e5151fb7f2769197d238a0cf1d16e9e52923c56
BrightAquilla <aquillabright@gmail.com>
2025-12-14 23:08:31 +0300

    updated add .py files

--- task_5.sql
+++ task_5.sql
@@ -2,5 +2,5 @@
 -- Insert a single row into the CUSTOMERS table
 -- The database will be passed as argument to mysql command
 
-INSERT INTO CUSTOMERS (customer_id, customer_name, email, address)
+INSERT INTO Customer(customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
