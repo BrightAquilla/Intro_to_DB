465a2de01b178ea2910e24f2bda8ebd6d9796445
BrightAquilla <aquillabright@gmail.com>
2025-12-14 23:15:33 +0300

    updated add .py files

--- task_4.sql
+++ task_4.sql
@@ -1,10 +1,4 @@
--- task_4.sql
--- Print full description of the table 'books'
--- The database will be passed as argument to mysql command
--- No DESCRIBE or EXPLAIN statements allowed
-
--- Use INFORMATION_SCHEMA to retrieve table details
 SELECT COLUMN_NAME, COLUMN_TYPE, IS_NULLABLE, COLUMN_KEY, COLUMN_DEFAULT, EXTRA
 FROM INFORMATION_SCHEMA.COLUMNS
-WHERE TABLE_NAME = 'BOOKS'
+WHERE TABLE_NAME = 'books'
 ORDER BY ORDINAL_POSITION;
